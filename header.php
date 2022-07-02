e2612b314dcf63c240534343015f8563250ecce5
alexandra spalato <alexaspalato@gmail.com>
2022-07-02 08:11:04 +0200

    menu

--- header.php
+++ header.php
@@ -2,7 +2,9 @@
 <!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]><html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]><html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js">
+<!--<![endif]-->
 
 <head>
     <meta charset="utf-8">
@@ -17,67 +19,81 @@
     <link rel="manifest" href="/manifest.json">
     <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#969494">
     <meta name="theme-color" content="#ffffff">
-    
+
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
 
     <script src="<?php bloginfo('stylesheet_directory'); ?>/js/top.min.js"></script>
-    
+
     <?php wp_head(); ?>
 </head>
 
 <?php if( is_page(array(1264, 10, 12, 14, 16, 18, 23, 25, 27, 5378)) ): ?>
-    <body <?php body_class('loading'); ?>>
-<?php else: ?>
+
+<body <?php body_class('loading'); ?>>
+    <?php else: ?>
+
     <body <?php body_class(); ?>>
-<?php endif; ?>
+        <?php endif; ?>
 
-    <div class="shifter-page">
-        <div class="stage">
-            <header class="site-header">
-                <div class="row">
-                    <div class="small-16 medium-6 columns">
-                        <div class="logo">
-                            <a href="<?php echo home_url(); ?>">
-                                <span class="span-one">
-                                    <img src="<?php echo IMAGES; ?>shelton-mindel.svg" alt="Shelton, Mindel &amp; Associates, Inc.">
-                                </span>
+        <div class="shifter-page">
+            <div class="stage">
+                <header class="site-header">
+                    <div class="row">
+                        <div class="small-16 medium-6 columns">
+                            <div class="logo">
+                                <a href="<?php echo home_url(); ?>">
+                                    <span class="span-one">
+                                        <img src="<?php echo IMAGES; ?>shelton-mindel.svg"
+                                            alt="Shelton, Mindel &amp; Associates, Inc.">
+                                    </span>
 
-                                <span class="span-two">
-                                    <img src="<?php echo IMAGES; ?>shelton-mindel.svg" alt="Shelton, Mindel &amp; Associates, Inc.">
-                                </span>
-                            </a>
+                                    <span class="span-two">
+                                        <img src="<?php echo IMAGES; ?>shelton-mindel.svg"
+                                            alt="Shelton, Mindel &amp; Associates, Inc.">
+                                    </span>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="small-4 medium-14 columns">
+                            <nav>
+                                <ul class="navigation">
+                                    <li class="desktop-link link"><a
+                                            class="<?php if( is_page(7) ){ echo 'current'; } ?>"
+                                            href="<?php echo get_permalink(7); ?>">Exhibitions</a></li>
+                                    <li class="desktop-link link"><a
+                                            class="<?php if( is_page(10) ){ echo 'current'; } ?>"
+                                            href="<?php echo get_permalink(10); ?>">Products</a></li>
+                                    <li class="desktop-link link"><a
+                                            class="<?php if( is_page(4552) ){ echo 'current'; } ?>"
+                                            href="<?php echo get_permalink(4552); ?>">Spaces</a></li>
+
+                                    <li class="desktop-link link has-subnav">
+                                        <a class="<?php if( is_page( array(12,14,16,18) ) ){ echo 'current'; } ?>"
+                                            href="javascript:void(0)">About <i class="fa fa-caret-down"></i></a>
+                                        <ul class="subnav">
+                                            <li class="subnav-link"><a
+                                                    class="<?php if( is_page(12) ){ echo 'current'; } ?>"
+                                                    href="<?php echo get_permalink(12); ?>">Profile</a></li>
+                                            <li class="subnav-link"><a
+                                                    class="<?php if( is_page(18) ){ echo 'current'; } ?>"
+                                                    href="<?php echo get_permalink(18); ?>">Publications</a></li>
+                                            <li class="subnav-link"><a
+                                                    class="<?php if( is_page(5378) ){ echo 'current'; } ?>"
+                                                    href="<?php echo get_permalink(5378); ?>">Careers</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="desktop-link link"><a
+                                            class="<?php if( is_page(27) ){ echo 'current'; } ?>"
+                                            href="<?php echo get_permalink(27); ?>">Contact</a></li>
+                                    <li class="mobile-link link">
+                                        <a id="open-mobile-menu" class="shifter-handle" href="#"></a>
+                                    </li>
+                                </ul>
+                            </nav>
                         </div>
                     </div>
-
-                    <div class="small-4 medium-14 columns">
-                        <nav>
-                            <ul class="navigation">
-                                <li class="desktop-link link"><a class="<?php if( is_page(7) ){ echo 'current'; } ?>" href="<?php echo get_permalink(7); ?>">Projects</a></li>
-                                <li class="desktop-link link"><a class="<?php if( is_page(10) ){ echo 'current'; } ?>" href="<?php echo get_permalink(10); ?>">Products</a></li>
-                                <li class="desktop-link link"><a class="<?php if( is_page(4552) ){ echo 'current'; } ?>" href="<?php echo get_permalink(4552); ?>">Spaces</a></li>
-
-                                <li class="desktop-link link has-subnav">
-                                    <a class="<?php if( is_page( array(12,14,16,18) ) ){ echo 'current'; } ?>" href="javascript:void(0)">About <i class="fa fa-caret-down"></i></a>
-                                    <ul class="subnav">
-                                        <li class="subnav-link"><a class="<?php if( is_page(12) ){ echo 'current'; } ?>" href="<?php echo get_permalink(12); ?>">Profile</a></li>
-                                        <li class="subnav-link"><a class="<?php if( is_page(14) ){ echo 'current'; } ?>" href="<?php echo get_permalink(14); ?>">Awards</a></li>
-                                        <li class="subnav-link"><a class="<?php if( is_page(18) ){ echo 'current'; } ?>" href="<?php echo get_permalink(18); ?>">Publications</a></li>
-                                        <li class="subnav-link"><a class="<?php if( is_page(5378) ){ echo 'current'; } ?>" href="<?php echo get_permalink(5378); ?>">Careers</a></li>
-                                    </ul>
-                                </li>
-                                
-                                <li class="desktop-link link"><a class="<?php if( is_page(16) ){ echo 'current'; } ?>" href="<?php echo get_permalink(16); ?>">Periodicals</a></li>
-                                <li class="desktop-link link"><a class="<?php if( is_page(25) ){ echo 'current'; } ?>" href="<?php echo get_permalink(25); ?>">Architect’s Eye</a></li>
-                                <li class="desktop-link link"><a class="<?php if( is_page(27) ){ echo 'current'; } ?>" href="<?php echo get_permalink(27); ?>">Contact</a></li>
-                                <li class="mobile-link link">
-                                    <a id="open-mobile-menu" class="shifter-handle" href="#"></a>
-                                </li>
-                            </ul>
-                        </nav>
-                    </div>
-                </div>
-            </header>
-    
+                </header>
--- template-projects.php
+++ template-projects.php
@@ -6,56 +6,56 @@ Template Name: Projects
 
 <?php get_header(); ?>
 
-    <div id="page-header">
-        <div class="row">
-            <div class="small-20 large-10 columns">
-                <h1><?php the_title(); ?></h1>
-            </div>
+<div id="page-header">
+    <div class="row">
+        <div class="small-20 large-10 columns">
+            <h1><?php the_title(); ?></h1>
+        </div>
 
-            <div class="small-20 large-10 columns">
-                <ul class="filter-grid small-block-grid-1 large-block-grid-2">
-                    <li>
-                        <div class="heading">
-                            <a id="category-selector" href="#">
-                                Category
-                            </a>
-                        </div>
+        <div class="small-20 large-10 columns">
+            <ul class="filter-grid small-block-grid-1 large-block-grid-2">
+                <li>
+                    <div class="heading">
+                        <a id="category-selector" href="#">
+                            Category
+                        </a>
+                    </div>
 
-                        <?php
+                    <?php
                             $types = get_terms('project-type', array(
                                 'orderby' => 'name',
                                 'hide_empty' => true,
                             ));
                         ?>
 
-                        <ul class="cats">
-                            <li><a data-type="*" href="#">View All</a></li>
-                            
-                            <?php foreach ($types as $type): ?>
-                                <li><a data-type="<?php echo $type->slug; ?>" href="#"><?php echo $type->name; ?></a></li>
-                            <?php endforeach; ?>
-                        </ul>
-                    </li>
+                    <ul class="cats">
+                        <li><a data-type="*" href="#">View All</a></li>
 
-                    <li>
-                        <div class="view-switcher">
-                            <div class="wrap">
-                                <a href="#" id="grid-view" class="current">Grid</a>/<a href="#" id="list-view">List</a>
-                            </div>
+                        <?php foreach ($types as $type): ?>
+                        <li><a data-type="<?php echo $type->slug; ?>" href="#"><?php echo $type->name; ?></a></li>
+                        <?php endforeach; ?>
+                    </ul>
+                </li>
+
+                <li>
+                    <div class="view-switcher">
+                        <div class="wrap">
+                            <a href="#" id="grid-view" class="current">Grid</a>/<a href="#" id="list-view">List</a>
                         </div>
-                    </li>
-                </ul>
-            </div>
+                    </div>
+                </li>
+            </ul>
         </div>
     </div>
+</div>
 
-    <div id="main">
-        <div class="row">
-            <div class="small-20 columns">
-                <ul class="project-grid grid-view small-block-grid-2 medium-block-grid-3 large-block-grid-4">
-                    <?php
+<div id="main">
+    <div class="row">
+        <div class="small-20 columns">
+            <ul class="project-grid grid-view small-block-grid-2 medium-block-grid-3 large-block-grid-4">
+                <?php
                         $args = array(
-                            'post_type' => 'project',
+                            'post_type' => 'exhibition',
                             'post_status' => 'publish',
                             'posts_per_page' => -1,
                         );
@@ -63,71 +63,71 @@ Template Name: Projects
                         $the_query = new WP_Query( $args );
                     ?>
 
-                    <?php if ( $the_query->have_posts() ) : ?>
-                        <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-                            <?php
+                <?php if ( $the_query->have_posts() ) : ?>
+                <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+                <?php
                                 $c = wp_get_post_terms($post->ID, 'project-type', array("fields" => "all"));
                                 $project_slug = $c[0]->slug;
                                 $project_cat = $c[0]->name
                             ?>
 
-                            <li class="proj <?php echo $project_slug; ?>">
-                                <a href="<?php echo get_permalink(); ?>">
-                                    <span class="grid-content">
-                                        <div class="thumbnail">
-                                            <?php if( get_field('thumbnail') ): ?>
-                                                <?php $img = wp_get_attachment_image_src( get_field('thumbnail'), 'grid-356x503' ); ?>
-                                                <img src="<?php echo $img[0]; ?>" width="356" height="503" alt="Thumbnail">
-                                            <?php endif; ?>
+                <li class="proj <?php echo $project_slug; ?>">
+                    <a href="<?php echo get_permalink(); ?>">
+                        <span class="grid-content">
+                            <div class="thumbnail">
+                                <?php if( get_field('thumbnail') ): ?>
+                                <?php $img = wp_get_attachment_image_src( get_field('thumbnail'), 'grid-356x503' ); ?>
+                                <img src="<?php echo $img[0]; ?>" width="356" height="503" alt="Thumbnail">
+                                <?php endif; ?>
+                            </div>
+
+                            <div class="title">
+                                <div class="heading"><?php the_title(); ?></div>
+                                <div class="project-category">
+                                    <?php echo $project_cat; ?>
+                                </div>
+                                <div class="project-code"><?php the_field('project_code'); ?></div>
+                            </div>
+                        </span>
+
+                        <span class="list-content">
+                            <div class="row">
+                                <div class="small-4 medium-3 columns">
+                                    <?php if( get_field('list_view_thumbnail') ): ?>
+                                    <?php $img = wp_get_attachment_image_src( get_field('list_view_thumbnail'), 'list-166x110' ); ?>
+                                    <div class="thumbnail" style="background-image: url(<?php echo $img[0]; ?>);"></div>
+                                    <?php endif; ?>
+                                </div>
+
+                                <div class="small-16 medium-16 columns">
+                                    <div class="row">
+                                        <div class="small-20 medium-8 columns adjust-width">
+                                            <div class="title"><?php the_title(); ?></div>
                                         </div>
 
-                                        <div class="title">
-                                            <div class="heading"><?php the_title(); ?></div>
-                                            <div class="project-category">
+                                        <div class="small-20 medium-7 columns">
+                                            <div class="cat">
                                                 <?php echo $project_cat; ?>
                                             </div>
-                                            <div class="project-code"><?php the_field('project_code'); ?></div>
                                         </div>
-                                    </span>
 
-                                    <span class="list-content">
-                                        <div class="row">
-                                            <div class="small-4 medium-3 columns">
-                                                <?php if( get_field('list_view_thumbnail') ): ?>
-                                                    <?php $img = wp_get_attachment_image_src( get_field('list_view_thumbnail'), 'list-166x110' ); ?>
-                                                    <div class="thumbnail" style="background-image: url(<?php echo $img[0]; ?>);"></div>
-                                                <?php endif; ?>
-                                            </div>
-                                            
-                                            <div class="small-16 medium-16 columns">
-                                                <div class="row">
-                                                    <div class="small-20 medium-8 columns adjust-width">
-                                                       <div class="title"><?php the_title(); ?></div> 
-                                                    </div>
-
-                                                    <div class="small-20 medium-7 columns">
-                                                       <div class="cat">
-                                                           <?php echo $project_cat; ?>
-                                                       </div> 
-                                                    </div>
-
-                                                    <div class="small-20 medium-5 columns">
-                                                        <div class="code">
-                                                            <?php the_field('project_code'); ?>
-                                                        </div>
-                                                    </div>
-                                                </div>
+                                        <div class="small-20 medium-5 columns">
+                                            <div class="code">
+                                                <?php the_field('project_code'); ?>
                                             </div>
                                         </div>
-                                    </span>   
-                                </a>
-                            </li>
-                        <?php endwhile; ?>
-                        <?php wp_reset_postdata(); ?>
-                    <?php else : endif; ?>
-                </ul>
-            </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </span>
+                    </a>
+                </li>
+                <?php endwhile; ?>
+                <?php wp_reset_postdata(); ?>
+                <?php else : endif; ?>
+            </ul>
         </div>
     </div>
-    
+</div>
+
 <?php get_footer(); ?>
